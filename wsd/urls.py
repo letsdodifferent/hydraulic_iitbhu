95b7a1b1ee59889877aa01d0e166a748a8f04c89
Aryan-0-1 <aryanchoudhary84010@gmail.com>
2025-02-05 22:31:03 +0530

    narander do your work

--- wsd/urls.py
+++ wsd/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.main,name='main'),
-    # path('',views.,name=''),
+    path('', views.main, name='main'),
+    path('generate_image/', views.wsd, name='wsd'),
 ]
